2eac3ef57262970a3ec266b77a73f9c2d9c1c4a1
chrisflodin <christopher.flodin@gmail.com>
2023-04-12 14:18:47 +0200

    be/fe-feat: styling & add comments to BE code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './styles/index.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import RoomPage from './pages/RoomPage'
 import { WebSocketProvider } from './shared/WebSocketContext'
+import Layout from './pages/HomePage/Layout'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <WebSocketProvider>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <HomePage />
+              </Layout>
+            }
+          />
           <Route path="/:id" element={<RoomPage />} />
         </Routes>
       </WebSocketProvider>
