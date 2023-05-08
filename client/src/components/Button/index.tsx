d69c751000f3cab782ded3be10537288125a3806
Martin Brolin <martinbrolin97@gmail.com>
2023-05-08 14:40:15 +0200

    added design for input and button component

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -1,10 +1,35 @@
 import styles from './Button.module.scss'
 
-export interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {}
+export enum ButtonColor {
+  Green = 'green',
+  Black = 'black',
+  White = 'white',
+}
+export interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  buttonColor?: ButtonColor
+}
+
+function mapColorToCSSClassName(color: ButtonColor | undefined) {
+  switch (color) {
+    case ButtonColor.Green:
+      return styles.green
+    case ButtonColor.Black:
+      return styles.black
+    case ButtonColor.White:
+      return styles.white
+    default:
+      return styles.green
+  }
+}
 
 export function Button(props: ButtonProps) {
   return (
-    <button {...props} className={`${props.className} ${styles.button}`}>
+    <button
+      {...props}
+      className={`${styles.button} 
+      ${mapColorToCSSClassName(props.buttonColor)} 
+      ${props.className ? props.className : ''}`}
+    >
       {props.children}
     </button>
   )
