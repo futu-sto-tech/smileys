941ee9d63594bc5fcd484e78f9c60b48cb761354
chrisflodin <christopher.flodin@gmail.com>
2023-05-23 13:19:54 +0200

    fe-fix: set correct url for laptop img

--- client/src/pages/HomePage/index.tsx
+++ client/src/pages/HomePage/index.tsx
@@ -48,7 +48,7 @@ function HomePage() {
       <Button className={styles.createAnewRoomButton} onClick={handleCreate}>
         Create a room
       </Button>
-      <img className={styles.laptop} src="../../public/assets/images/laptop.png" />
+      <img className={styles.laptop} src="../assets/images/laptop.png" />
       <h1 className={`${styles.heading} ${styles.getStarted}`}>Get started with your team</h1>
       <div className={styles.getStartedContainer}>
         <div className={styles.step}>
