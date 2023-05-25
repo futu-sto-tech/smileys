a2cd36262877eeb54008c780adfd9278d7aa6ee8
Martin Brolin <martinbrolin97@gmail.com>
2023-05-25 16:46:33 +0200

    removed a slash

--- client/src/consts/urls.ts
+++ client/src/consts/urls.ts
@@ -7,7 +7,7 @@ const GIPHY_BASE_URL_TRENDING = 'https://api.giphy.com/v1/gifs/trending'
 const BASE_SERVER_URL = VITE_SERVER_ADDRESS
 
 export const SERVER_URLS = {
-  SESSION_EXISTS: BASE_SERVER_URL + '/session-exists',
+  SESSION_EXISTS: BASE_SERVER_URL + 'session-exists',
 }
 
 export const GIPHY_URLS = {
