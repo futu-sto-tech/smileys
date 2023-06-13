eac72f9e3516fd4a1a28fd50c2da9db6571b93cd
Martin Brolin <martinbrolin97@gmail.com>
2023-06-13 16:00:15 +0200

    fe-fix: searchbar icons

--- client/src/pages/Layout/Footer/FooterSocialLinks.tsx
+++ client/src/pages/Layout/Footer/FooterSocialLinks.tsx
@@ -1,12 +1,6 @@
 import styling from './FooterSocialLinks.module.scss'
 import footerStyling from './index.module.scss'
-import {
-  SmileyLogo,
-  InstagramLogo,
-  TikTokLogo,
-  YouTubeLogo,
-  TwitterLogo,
-} from '../../../components/CodeTextField/Logos/Logos'
+import { SmileyLogo, InstagramLogo, TikTokLogo, YouTubeLogo, TwitterLogo } from '../../../components/SVGs/Logos'
 
 const { footerTextStyling } = footerStyling
 const { container, socialLinksContainerStyling, elementMargin } = styling
