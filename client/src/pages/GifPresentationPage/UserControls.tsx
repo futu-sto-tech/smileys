974bfa9a76e72ab980aeafb38f4fc564cc693055
Martin Brolin <martinbrolin97@gmail.com>
2023-06-14 12:54:06 +0200

    fe-feat: endpage

--- client/src/pages/GifPresentationPage/UserControls.tsx
+++ client/src/pages/GifPresentationPage/UserControls.tsx
@@ -3,6 +3,7 @@ import { Button } from '../../components/Button'
 import { IAppProvider } from '../../types/AppContext'
 import AppContext from '../../shared/AppContext'
 import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 interface UserControlsProps {
   isCreator: boolean
@@ -10,12 +11,19 @@ interface UserControlsProps {
 
 function UserControls({ isCreator }: UserControlsProps) {
   const { deleteSession }: IAppProvider = useContext(AppContext)
+  const navigate = useNavigate()
   const restrictToCreator = isCreator ? 'block' : 'none'
+
+  function handleEndSession() {
+    deleteSession()
+    navigate('/end')
+  }
+
   return (
     <div className={styles.userControlContainer}>
       {/* <Button>Button</Button>
       <Button>Button</Button> */}
-      <Button style={{ display: restrictToCreator }} buttonColor="red" onClick={deleteSession}>
+      <Button style={{ display: restrictToCreator }} buttonColor="red" onClick={handleEndSession}>
         End Session
       </Button>
     </div>
