85ec2f64232af6d9f63d466f3e234e723dab1bb0
Martin Brolin <martinbrolin97@gmail.com>
2023-06-20 11:59:22 +0200

    fe/be-chore: rewrite /session-exists to allow empty rooms

--- client/src/services/server.ts
+++ client/src/services/server.ts
@@ -4,13 +4,13 @@ import { handleAxiosMethod } from '../utils/handleAxiosMethod'
 import { Session } from '../types/types'
 import { ApiError } from '../types/errors'
 
-export const checkIfSessionExists = async (roomId: string): Promise<{ err: ApiError | null; data: Session | null }> => {
+export const checkIfSessionExists = async (roomId: string): Promise<{ err: ApiError | null; data: boolean | null }> => {
   const requestConfig: AxiosRequestConfig = {
     method: 'post',
     url: SERVER_URLS.SESSION_EXISTS,
     data: roomId,
   }
 
-  const res = await handleAxiosMethod<Session>(requestConfig)
+  const res = await handleAxiosMethod<boolean>(requestConfig)
   return res
 }
