34ae317eb68b18d7b04038b14ae568feb1c9a673
Martin Brolin <martinbrolin97@gmail.com>
2023-06-28 11:18:07 +0200

    fe-fix: Google Analytics

--- client/src/pages/Layout/index.tsx
+++ client/src/pages/Layout/index.tsx
@@ -6,6 +6,9 @@ import ResolutionNotSupported from './ResolutionNotSupported'
 import useResolutionSupported from '../../hooks/useResolutionSupported'
 import { useEffect } from 'react'
 import CookieBanner from '../../components/CookieBanner'
+import useLocalStorage from 'use-local-storage'
+import { TRACKING_ID } from '../../consts/envs'
+import ReactGA from 'react-ga4'
 
 const { navStyle, footerStyle, contentStyle, mainStyle } = styling
 
@@ -18,14 +21,17 @@ function Layout() {
   const resolutionSupported = useResolutionSupported(1000)
   if (!resolutionSupported) return <ResolutionNotSupported />
 
+  // Google analytics
+  const [allowTracking, _] = useLocalStorage<boolean>('allowTracking', false)
+
   useEffect(() => {
+    if (allowTracking && TRACKING_ID) ReactGA.send({ hitType: 'pageview', page: window.location.pathname })
     window.scrollTo({ top: 0 })
   }, [location])
 
   return (
     <>
       <CookieBanner />
-      <GoogleAnalytics />
       <main className={mainStyle}>
         <NavBar />
         <div className={contentStyle}>
