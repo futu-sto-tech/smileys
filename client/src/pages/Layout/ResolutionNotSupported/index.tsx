185e28e2a40386e41aafca180601636f39354302
chrisflodin <christopher.flodin@gmail.com>
2023-07-08 17:00:47 +0200

    fe-feat: update warning on small screen sizes

--- client/src/pages/Layout/ResolutionNotSupported/index.tsx
+++ client/src/pages/Layout/ResolutionNotSupported/index.tsx
@@ -3,7 +3,9 @@ import styling from './index.module.scss'
 function ResolutionNotSupported() {
   return (
     <div className={styling.notSupported}>
-      <h1>This screen size is not supported.</h1>
+      <h1>
+        We only support desktop screen sizes... <span className='text-[30px]'>😢</span>
+      </h1>
     </div>
   )
 }
